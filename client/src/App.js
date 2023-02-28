7866ef2ea415a983b830dd5eae669d64018fe7da
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-02-28 22:54:22 +0200

    A dark mode has been added to the CSS style of material UI with redux state

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
 
 function App() {
+  // Get the selected mode from the global Redux store
+
+  const mode = useSelector((state) => state.global.mode);
+  // Generate a new theme object using the selected mode
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  // Render the app with the selected theme and a baseline CSS reset
+
   return (
     <div className="App">
-     
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
 
 export default App;
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state"; // This is the reducer from state\index.js (jsconfig is set to src)
+import { Provider } from "react-redux";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Configure the Redux store with a single reducer for the "global" state slice
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
+
+// Render the app using ReactDOM.createRoot and the Redux store provider
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
