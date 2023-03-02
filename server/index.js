63ef1d3528fdbe84907d7d09a0978cea1f659e49
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-02 08:19:24 +0200

    User models are initialized on the server side with MongoDB

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
 
+// data imports
+import User from './models/User.js';
+import {dataUser} from './data/index.js';
 
 
 /* CONFIGURATION */
@@ -40,4 +43,8 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(() => {
     mongoose.set('strictQuery', false);
     app.listen(PORT, () =>   console.log(`Server running on port: ${PORT}`));
+
+    /* ONLY ADD DATA ONE TIME */
+    User.insertMany(dataUser);
+
 }).catch((error) => console.log(`${error} did not connect`))
