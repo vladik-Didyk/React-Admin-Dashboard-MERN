87e125fac85b413e2b55fff165f056e14e64739d
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-07 14:27:39 +0200

    Route of overall statistics of sales have been created (BackEnd)

--- server/index.js
+++ server/index.js
@@ -15,7 +15,14 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js";
+import OverallStat from "./models/OverallStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+} from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -46,9 +53,10 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
     /* ONLY ADD DATA ONE TIME */
+    // OverallStat.insertMany(dataOverallStat);
     // Product.insertMany(dataProduct);
     //  ProductStat.insertMany(dataProductStat);
-     // Transaction.insertMany(dataTransaction);
+    // Transaction.insertMany(dataTransaction);
     //  User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
