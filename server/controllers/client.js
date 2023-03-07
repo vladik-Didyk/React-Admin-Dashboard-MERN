abfcd4e640a0a43e7d310643126b9b7bbdc5e839
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-03-07 10:20:32 +0200

    Transaction routing and pagination sorting have been implemented on the server

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,7 +1,8 @@
-// Import the Product and ProductStat models
+// Import the Product, ProductStat, User, and Transaction models
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 // Define an asynchronous function that retrieves all products with their corresponding statistics
 export const getProducts = async (req, res) => {
@@ -9,14 +10,14 @@ export const getProducts = async (req, res) => {
     // Retrieve all products from the database
     const products = await Product.find();
 
-    // Map over each product to retrieve its corresponding statistics
+    // Retrieve the statistics for each product
     const productsWithStats = await Promise.all(
       products.map(async (product) => {
-        // Retrieve the ProductStat object that matches the product's ID
+        // Find the corresponding statistics for the current product
         const stat = await ProductStat.find({
           productId: product._id,
         });
-        // Return an object that includes the product's original data and the statistics object
+        // Return an object that includes the original product data and its corresponding statistics
         return {
           ...product._doc,
           stat,
@@ -25,22 +26,65 @@ export const getProducts = async (req, res) => {
     );
 
     // Send the response to the client with a 200 status code and an array of products with statistics
-    res.status(200).json(productsWithStats); 
+    res.status(200).json(productsWithStats);
   } catch (error) {
     // If an error occurs, send the response to the client with a 404 status code and an error message
     res.status(404).json({ message: error.message });
   }
 };
 
+// Define an asynchronous function that retrieves all customers with the "user" role
 export const getCustomers = async (req, res) => {
-
   try {
-    const customers = await User
-    .find({role: "user"})
-    .select("-password");
+    // Find all users with the role "user" and exclude the password field
+    const customers = await User.find({ role: "user" }).select("-password");
+    // Send the response to the client with a 200 status code and an array of customers
     res.status(200).json(customers);
+  } catch (error) {
+    // If an error occurs, send the response to the client with a 404 status code and an error message
+    res.status(404).json({ message: error.message });
+  }
+};
 
-   
+// Define an asynchronous function that retrieves transactions with pagination and search/sorting options
+export const getTransactions = async (req, res) => {
+  try {
+    // Get the pagination, search, and sorting options from the query parameters
+    const { page = 1, pageSize = 10, sort = null, search = "" } = req.query;
+
+    // Define a function to generate the formatted sort object
+    const generateSort = () => {
+      const sortParsed = JSON.parse(sort);
+      const sortFormatted = {
+        [sortParsed.field]: sortParsed.sort === "desc" ? -1 : 1,
+      };
+      return sortFormatted;
+    };
+
+    // Generate the formatted sort object
+    const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+    // Find transactions matching the search criteria, sort and paginate the results
+    const transactions = await Transaction.find({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    })
+      .sort(sortFormatted)
+      .skip(page * pageSize)
+      .limit(pageSize);
+
+    // Count the total number of transactions matching the search criteria
+    const total = await Transaction.countDocuments({
+      name: { $regex: search, $options: "i" },
+    });
+
+    // Send the response to the client with a 200 status code and an object containing the transactions and total count
+    res.status(200).json({
+      transactions,
+      total,
+    });
   } catch (error) {
     // If an error occurs, send the response to the client with a 404 status code and an error message
     res.status(404).json({ message: error.message });
